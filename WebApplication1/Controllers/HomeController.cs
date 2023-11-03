708fcdf7c48d20cb1fa03b21e1d0d5bc2f62734f
ANHELNANA <124913560+ANHELNANA@users.noreply.github.com>
2023-11-03 15:40:34 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult AdminDashboard()
+        {
+            return View(); // 這個操作方法對應到 Admin 儀表板頁面
+        }
+
+        public ActionResult ManagerDashboard()
+        {
+            return View(); // 這個操作方法對應到 Manager 儀表板頁面
+        }
+        public ActionResult PlayerDashboard()
+        {
+            return View(); // 這個操作方法對應到 Player 儀表板頁面
+        }
+    }
+}
---
+++ WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        private NEW2101Entities1 db = new NEW2101Entities1();
+       
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Account, string Password)
+        {
+            
+            var existingUser = db.AccountData.FirstOrDefault(u => u.Account == Account && u.Password == Password);
+            
+
+            //ViewBag.Title = "JJHu out";
+
+            if (existingUser != null)
+            {
+
+                Session["Username"] = existingUser.Username;
+                Session["User_ID"] = existingUser.ID;
+                Session["Role_ID"] = existingUser.Role_ID;
+                Session["Logtime"] = DateTime.Now.ToString();
+
+                // Session["Account"] = existingUser.Account;
+
+                //
+
+
+                //var Username = existingUser.Username;
+                //ViewBag.Username = existingUser.Username;
+                //Console.WriteLine(ViewBag.Username);
+                if (existingUser.Role_ID == 1) // Admin
+                {
+                   
+                    
+                    return RedirectToAction("AdminDashboard", "Home");
+
+                }
+                else if (existingUser.Role_ID == 2) // Manager
+                {
+                    Debug.WriteLine("User is a Manager.");
+                    return RedirectToAction("ManagerDashboard", "Home");
+
+                }
+                else if (existingUser.Role_ID == 3) // Player
+                {
+
+                    Debug.WriteLine("User is a Player.");
+                    return RedirectToAction("PlayerDashboard", "Home");
+
+                }
+              
+            }
+
+
+            ModelState.AddModelError("", "帳號或密碼輸入錯誤，請重新輸入！！");
+            return View("Index");
+
+
+
+        }
+       
+    }
+}
